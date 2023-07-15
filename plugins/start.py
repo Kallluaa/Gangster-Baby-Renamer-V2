6e675100a1c8e2d5492ab6ed736f46184bbf00d7
Dinobonecrash <121476035+Dinobonecrash@users.noreply.github.com>
2023-07-15 17:34:59 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -138,9 +138,9 @@ async def send_doc(client, message):
     c_time = time.time()
 
     if user_type == "Free":
-        LIMIT = 600
+        LIMIT = 10
     else:
-        LIMIT = 50
+        LIMIT = 00
     then = used_date + LIMIT
     left = round(then - c_time)
     conversion = datetime.timedelta(seconds=left)
