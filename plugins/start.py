88cd860132176f874cdd2d66c255f1343d9b5f59
Kallluaa <136989645+Kallluaa@users.noreply.github.com>
2023-07-18 00:05:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -52,7 +52,7 @@ async def start(client, message):
                                 reply_markup=InlineKeyboardMarkup(
                                       [[InlineKeyboardButton("🔺 Update Channel 🔺", url="https://t.me/primax_movie")], 
                                       [InlineKeyboardButton("Support Group", url='https://t.me/discuss_on'),
-                                      InlineKeyboardButton("Movie Channel", url='https://t.me/primax_hd']]) 
+                                      InlineKeyboardButton("Movie Channel", url='https://t.me/primax_hd')]] 
 	  
         return
     if id:
